1a175932c0b33c72f5b7856fdea484f6abd2b841
Elena <sokoliklena@ukr.net>
2014-03-22 20:04:27 +0200

    hello, diploma

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Diploma::Application.config.secret_key_base = '4be3b10aca5e4640c9a5b7fce3a7c6ec183209b659128861610e985ca19b8097892a47ec6f211307c48b7a58531aad330e88c7853bc523bdb8ef91a050a496ca'
